669601bca87a4ef890506b5f216e8793adc36aad
TanPhan1112 <minhtan111293@gmail.com>
2025-10-06 15:33:41 +0700

    Update contact information in Contact component

--- src/screens/Contact/Contact.jsx
+++ src/screens/Contact/Contact.jsx
@@ -5,8 +5,8 @@ function Contact() {
     return (
         <div id="contact" className="contact-container">
             <p>Duc Minh Tan Phan</p>
-            <p>Saskatoon, SK, Canada</p>
-            <p>phone:&nbsp;<a href="tel:+16044413896">+1 (604) 441-3896</a></p>
+            <p>Ho Chi Minh, Vietnam</p>
+            <p>phone:&nbsp;<a href="tel:+84867052405">+84 867-052-405</a></p>
             <p>Email:&nbsp;<a href="mailto:minhtan111293@gmail.com">minhtan111293@gmail.com</a></p>
         </div>
     )
